8b3b34b0bbbaca404ef2b7460999affa25dd82c0
Arden's Student Account <91262831+Arden007@users.noreply.github.com>
2023-11-12 20:53:27 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,59 @@
 #!/usr/bin/node
+/**
+ * A script that prints all characters of a Star Wars movie.
+ */
 
 const request = require('request');
-
-const baseUrl = 'https://swapi-api.alx-tools.com/api/';
 
 /**
  * Retrieves and prints the characters of a Star Wars movie.
  * @param {number} movieId - The ID of the Star Wars movie.
  */
-function getMovieCharacters(movieId) {
+async function getMovieCharacters(movieId) {
+  const baseUrl = 'https://swapi-api.hbtn.io/api/';
   const filmUrl = `${baseUrl}films/${movieId}/`;
 
-  request(filmUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      const filmData = JSON.parse(body);
-      const charactersUrls = filmData.characters;
+  try {
+    // Send a request to the SWAPI to get movie data
+    const filmData = await makeRequest(filmUrl);
 
-      charactersUrls.forEach((characterUrl) => {
-        request(characterUrl, (charError, charResponse, charBody) => {
-          if (charError) {
-            console.error('Error:', charError);
-          } else {
-            const characterData = JSON.parse(charBody);
-            console.log(characterData.name);
-          }
-        });
-      });
+    // Extract the URLs of the characters from the movie data
+    const charactersUrls = filmData.characters;
+
+    // For each character URL, send a request to get character data
+    for (const characterUrl of charactersUrls) {
+      const characterData = await makeRequest(characterUrl);
+      console.log(characterData.name);
     }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+/**
+ * Makes an HTTP request and returns a Promise.
+ * @param {string} url - The URL to make the request to.
+ * @returns {Promise} - A promise that resolves with the parsed JSON response.
+ */
+function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const data = JSON.parse(body);
+        resolve(data);
+      }
+    });
   });
 }
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+// Retrieve movie ID from command line arguments and call the function
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: ./refactored-starwars_characters.js <movie_id>');
   process.exit(1);
 }
 
-const movieId = process.argv[2];
 getMovieCharacters(movieId);
