7a11a80d217892cffd1b0a9a34fe1b6f7f5bf4ca
Arden's Student Account <91262831+Arden007@users.noreply.github.com>
2023-11-12 19:35:27 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,41 +3,29 @@
     A script that prints all characters of a Star Wars movie:
 */
 
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 3) {
-  console.error("Usage: ./0-starwars_characters.js <movie_id>");
-  process.exit(1);
-}
+const request = require('request');
 
-const request = require("request");
-
-const baseUrl = "https://swapi-api.alx-tools.com/api/";
+const baseUrl = 'https://swapi-api.alx-tools.com/api/';
 
 /**
  * Retrieves and prints the characters of a Star Wars movie.
  * @param {number} movieId - The ID of the Star Wars movie.
  */
 function getMovieCharacters(movieId) {
-  // Construct the URL for the specified movie
   const filmUrl = `${baseUrl}films/${movieId}/`;
 
-  // Send a request to the SWAPI to get movie data
   request(filmUrl, (error, response, body) => {
     if (error) {
-      console.error("Error:", error);
+      console.error('Error:', error);
     } else {
-      // Parse the response body into JSON
       const filmData = JSON.parse(body);
-      // Extract the URLs of the characters from the movie data
       const charactersUrls = filmData.characters;
 
-      // For each character URL, send a request to get character data
       charactersUrls.forEach((characterUrl) => {
         request(characterUrl, (charError, charResponse, charBody) => {
           if (charError) {
-            console.error("Error:", charError);
+            console.error('Error:', charError);
           } else {
-            // Parse the character data and print the character name
             const characterData = JSON.parse(charBody);
             console.log(characterData.name);
           }
@@ -47,6 +35,10 @@ function getMovieCharacters(movieId) {
   });
 }
 
-// Retrieve movie ID from command line arguments and call the function
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
 const movieId = process.argv[2];
 getMovieCharacters(movieId);
