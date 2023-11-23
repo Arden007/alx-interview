a24365d6dd90ec63831acda4ba29b6f4df34a2a0
Arden007 <cloveryuno664@gmail.com>
2023-11-23 23:21:49 +0200

    greedy sort

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,11 +19,12 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    else:
+        coin = sorted(coins, reverse=True)
+        counter = 0
+        for coin_value in coin:
+            while total >= coin_value:
+                counter += total // coin_value
+                total %= coin_value
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        return counter if total == 0 else -1
