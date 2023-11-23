2b212ef57c763090f076dcc598e40e854e2f045a
Arden007 <cloveryuno664@gmail.com>
2023-11-23 23:03:36 +0200

    task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module determines the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest number
+    of coins needed to meet a given amount total.
+
+    :param coins: List of coin values
+    :type coins: List[int]
+    :param total: Total amount to be achieved
+    :type total: int
+    :return: Fewest number of coins needed to meet total
+    :rtype: int
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
